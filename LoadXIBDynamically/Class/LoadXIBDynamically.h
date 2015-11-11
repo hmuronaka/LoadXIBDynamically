033969bea4546a277e8f5cb2b904c8fce7d8bdd1
hmuronaka <capriccio27@gmail.com>
2015-11-11 18:15:52 +0900

    Add header

---
+++ LoadXIBDynamically/Class/LoadXIBDynamically.h
@@ -0,0 +1,14 @@
+//
+//  LoadXIBDynamically.h
+//  LoadXIBDynamically
+//
+//  Created by Muronaka Hiroaki on 2015/11/11.
+//  Copyright © 2015年 Muronaka Hiroaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "AppDelegate+LoadXIBDynamically.h"
+#import "UINib+LoadXIBDynamically.h"
+#import "UIViewController+LoadXIBDynamically.h"
+#import "LXDViewController.h"
